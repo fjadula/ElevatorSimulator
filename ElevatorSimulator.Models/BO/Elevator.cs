074382448cfe0b064053588bacb2cf4bd9321dcb
FezileJ <fezile.jadula@sage.com>
2023-06-26 20:18:56 +0200

    Add project files.

---
+++ ElevatorSimulator.Models/BO/Elevator.cs
@@ -0,0 +1,39 @@
+﻿using ElevatorSimulator.Models.Enums;
+using System;
+
+namespace ElevatorSimulator.Models.BO
+{
+  public class Elevator
+  {
+    private static int nextId = 1;
+
+    public int Id { get; }
+    public int CurrentFloor { get; set; }
+    public Direction Direction { get; set; }
+    public int PeopleCount { get; set; }
+    public TimeSpan ExpectedTimeOfArrival { get; set; }
+    public ElevatorStatus Status { get; set; }
+    public int WeightLimit { get; set; }
+
+    public Elevator()
+    {
+      Id = nextId++;
+      CurrentFloor = 1;
+      Status = ElevatorStatus.Operational;
+      Direction = Direction.Stationary;
+      WeightLimit = 10; // Default weight limit of 10 people
+    }
+
+    public void ShowStatus()
+    {
+      Console.WriteLine("Elevator Status:");
+      Console.WriteLine($"ID: {Id}");
+      Console.WriteLine($"Current Floor: {CurrentFloor}");
+      Console.WriteLine($"Direction: {Direction}");
+      Console.WriteLine($"People Count: {PeopleCount}");
+      Console.WriteLine($"Expected Time of Arrival: {ExpectedTimeOfArrival}");
+      Console.WriteLine($"Status: {Status}");
+      Console.WriteLine($"Weight Limit: {WeightLimit} people");
+    }
+  }
+}
---
+++ ElevatorSimulator/Program.cs
@@ -0,0 +1,119 @@
+﻿using ElevatorSimulator.Models.BO;
+using ElevatorSimulator.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ElevatorSimulator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      List<Elevator> elevators = new List<Elevator>();
+      elevators.Add(new Elevator());
+      elevators.Add(new Elevator());
+      elevators.Add(new Elevator());
+
+      int choice = 0;
+
+      do
+      {
+        Console.WriteLine("Elevator Simulator Menu");
+        Console.WriteLine("1. Show Status of Elevator");
+        Console.WriteLine("2. Call Elevator");
+        Console.WriteLine("3. Set Status of Elevator");
+        Console.WriteLine("4. Exit Program");
+
+        Console.WriteLine("Enter your choice (1-4):");
+        choice = Convert.ToInt32(Console.ReadLine());
+        Elevator selectedElevator;
+        switch (choice)
+        {
+          case 1:
+            Console.WriteLine("Elevator Status:");
+            for (int i = 0; i < elevators.Count; i++)
+            {
+              Console.WriteLine($"Elevator {i + 1} Status:");
+              elevators[i].ShowStatus();
+              Console.WriteLine();
+            }
+            break;
+          case 2:
+            selectedElevator = elevators.FirstOrDefault(e => e.Status == ElevatorStatus.Operational);
+            if (selectedElevator != null)
+            {
+              Console.WriteLine("Enter the destination floor number:");
+              int destinationFloor = Convert.ToInt32(Console.ReadLine());
+
+              Console.WriteLine("How many people are getting on?");
+              int peopleGettingOn = Convert.ToInt32(Console.ReadLine());
+
+              Console.WriteLine("How many people are waiting on the destination floor?");
+              int peopleWaiting = Convert.ToInt32(Console.ReadLine());
+
+              // Check if the number of people getting on exceeds the weight limit
+              if (peopleGettingOn > selectedElevator.WeightLimit)
+              {
+                Console.WriteLine("Weight limit exceeded. Cannot accommodate that many people.");
+                break;
+              }
+
+              // Update elevator properties accordingly
+              selectedElevator.CurrentFloor = destinationFloor;
+              selectedElevator.PeopleCount += peopleGettingOn;
+              // Update other elevator properties as needed
+
+              Console.WriteLine($"Elevator {selectedElevator.Id} called successfully!");
+            }
+            else
+            {
+              Console.WriteLine("No operational elevators available. Please try again later.");
+            }
+            break;
+
+          case 3:
+            Console.WriteLine("Enter the elevator number:");
+            int elevatorNum = Convert.ToInt32(Console.ReadLine());
+            if (elevatorNum > 0 && elevatorNum <= elevators.Count)
+            {
+              selectedElevator = elevators[elevatorNum - 1];
+
+              Console.WriteLine("Enter the new status of the elevator:");
+              Console.WriteLine("1. Operational");
+              Console.WriteLine("2. OutOfOrder");
+              int statusChoice = Convert.ToInt32(Console.ReadLine());
+
+              switch (statusChoice)
+              {
+                case 1:
+                  selectedElevator.Status = ElevatorStatus.Operational;
+                  Console.WriteLine($"Elevator {elevatorNum} status updated to Operational successfully!");
+                  break;
+                case 2:
+                  selectedElevator.Status = ElevatorStatus.OutOfOrder;
+                  Console.WriteLine($"Elevator {elevatorNum} status updated to OutOfOrder successfully!");
+                  break;
+                default:
+                  Console.WriteLine("Invalid status choice. Please try again.");
+                  break;
+              }
+            }
+            else
+            {
+              Console.WriteLine("Invalid elevator number. Please try again.");
+            }
+            break;
+          case 4:
+            Console.WriteLine("Exiting the program...");
+            break;
+          default:
+            Console.WriteLine("Invalid choice. Please try again.");
+            break;
+        }
+
+        Console.WriteLine();
+      } while (choice != 4);
+    }
+  }
+}
