650f40a4923de3a8741d87e356526dad5ce3742c
FezileJ <fezile.jadula@sage.com>
2023-06-27 02:52:02 +0200

    Adding folder an structures

---
+++ ElevatorSimulator.Models/DTO/ElevatorDTO.cs
@@ -0,0 +1,16 @@
+﻿using ElevatorSimulator.Models.Enums;
+
+namespace ElevatorSimulator.DTOs
+{
+  public class ElevatorDTO
+  {
+    public int Id { get; set; }
+    public int CurrentFloor { get; set; }
+    public int DestinationFloor { get; set; }
+    public Direction Direction { get; set; }
+    public int PeopleCount { get; set; }
+    public TimeSpan ExpectedTimeOfArrival { get; set; }
+    public ElevatorStatus Status { get; set; }
+    public int WeightLimit { get; set; }
+  }
+}
